2f2a7bc41d907ede7e793a26e2fbf4eff31a1c79
bushraYounes <boushra.ali.younes@gmail.com>
2024-06-20 18:22:02 +0300

    landmarkControllerV1

--- app/Http/Controllers/LandmarkController.php
+++ app/Http/Controllers/LandmarkController.php
@@ -2,42 +2,74 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Traits\ApiResponseTrait;
+use App\Models\Image;
 use App\Models\Landmark;
-use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
+use App\Http\Traits\ApiResponseTrait;
+use App\Http\Traits\FileStorageTrait;
+use App\Http\Resources\LandmarkResource;
+use App\Http\Requests\LandmarkStoreRequest;
+use App\Http\Requests\LandmarkUpdateRequest;
 
 class LandmarkController extends Controller
 {
-     use ApiResponseTrait;
+    use ApiResponseTrait, FileStorageTrait;
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
         try {
-            $landmark = Landmark::paginate();
-            return $this->successResponse($landmark, 'Done', 200);
-            // return $this->paginated($landmark, 'Done', 200);
+            $landmarks = Landmark::with('city')->paginate(9);
+            return $this->successResponse(LandmarkResource::collection($landmarks), 'Done', 200);
         } catch (\Throwable $th) {
             Log::error($th);
-            return $this->errorResponse(null,"there is something wrong in server",500);
+            return $this->errorResponse(null, "there is something wrong in server", 500);
         }
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(LandmarkStoreRequest $request)
     {
         try {
+            DB::beginTransaction();
+
+            // store Images
+            // $internal_image = $this->storeFile($request->internal_image, 'landmark/' . $request->name . '/internal_image');
+            // $external_image = $this->storeFile($request->external_image, 'landmark/' . $request->name . '/external_image');
+
+            $internal_image = $this->storeFile($request->internal_image, 'landmark');
+            $external_image = $this->storeFile($request->external_image, 'landmark');
+
+
             $landmark = Landmark::create([
-                //
+                'name'                  =>  $request->name,
+                'city_id'               =>  $request->city_id,
+                'location'              =>  $request->location,
+                'primary_description'   =>  $request->primary_description,
+                'secondary_description' =>  $request->secondary_description,
+                'internal_image'        =>  $internal_image,
+                'external_image'        =>  $external_image,
             ]);
-            return $this->successResponse($landmark, 'Created Successfuly', 200);
+
+
+            // connect images in Image model with Landmark model
+            foreach ($request->file('images') as $image) {
+                Image::create([
+                    'path' => $this->storeFile($image, 'landmark'),
+                    'imageable_type' => Landmark::class,
+                    'imageable_id' => $landmark->id,
+                ]);
+            }
+            DB::commit();
+            return $this->successResponse(new LandmarkResource($landmark), 'Created Successfuly', 200);
         } catch (\Throwable $th) {
             Log::error($th);
-            return $this->errorResponse(null,"there is something wrong in server",500);
+            DB::rollBack();
+            return $this->errorResponse(null, "there is something wrong in server", 500);
         }
     }
 
@@ -47,25 +79,94 @@ class LandmarkController extends Controller
     public function show(Landmark $landmark)
     {
         try {
-            return $this->successResponse($landmark, 'Done', 200);
+            return $this->successResponse(new LandmarkResource($landmark), 'Done', 200);
         } catch (\Throwable $th) {
             Log::error($th);
-            return $this->errorResponse(null,"there is something wrong in server",500);
+            return $this->errorResponse(null, "there is something wrong in server", 500);
         }
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Landmark $landmark)
+    public function update(LandmarkUpdateRequest $request, Landmark $landmark)
     {
         try {
-            // $landmark->nn = $request->input('nn') ?? $landmark->nn;
-            $landmark->save();
-            return $this->successResponse($landmark, ' Updated Successfuly', 200);
+            $newData = [];
+
+            if (isset($request->name)) {
+                $newData['name'] = $request->name;
+            }
+            if (isset($request->city_id)) {
+                $newData['city_id'] = $request->city_id;
+            }
+            if (isset($request->location)) {
+                $newData['location'] = $request->location;
+            }
+
+            if (isset($request->primary_description)) {
+                $newData['primary_description'] = $request->primary_description;
+            }
+            if (isset($request->secondary_description)) {
+                $newData['secondary_description'] = $request->secondary_description;
+            }
+
+
+            if (isset($request->internal_image)) {
+                $this->deleteImage($landmark->internal_image, storage_path('app\public\landmark'));
+
+                //save new image
+                $image_path = $this->storeFile($request->internal_image, 'landmark');
+
+                //add new image to list
+                $newData['internal_image'] = $image_path;
+            }
+
+            if (isset($request->external_image)) {
+                $this->deleteImage($landmark->external_image, storage_path('app\public\landmark'));
+
+
+                //save new image
+                $image_path = $this->storeFile($request->external_image, 'landmark');
+
+                //add new image to list
+                $newData['external_image'] = $image_path;
+            }
+
+            if (isset($request->images)) {
+
+                //delete the old list
+                foreach ($landmark->images as $image) {
+
+                    // delete image from storage
+                    $this->deleteImage($image->path, storage_path('app\public\landmark'));
+
+                    // delete image from Image 
+                    $image->delete();
+                }
+                // $landmark->deleteImages();
+
+
+                // add the new list
+                foreach ($request->file('images') as $image) {
+                    $image_path = $this->storeFile($image, 'landmark');
+
+                    Image::create([
+                        'path' => $image_path,
+                        'imageable_type' => Landmark::class,
+                        'imageable_id' => $landmark->id,
+                    ]);
+
+                    $newData['images'][] = $image_path;
+                }
+            }
+
+            $landmark->update($newData);
+
+            return $this->successResponse(new LandmarkResource($landmark), ' Updated Successfuly', 200);
         } catch (\Throwable $th) {
             Log::error($th);
-            return $this->errorResponse(null,"there is something wrong in server",500);
+            return $this->errorResponse(null, "there is something wrong in server", 500);
         }
     }
 
@@ -75,11 +176,21 @@ class LandmarkController extends Controller
     public function destroy(Landmark $landmark)
     {
         try {
+
+            $this->deleteImage($landmark->internal_image, storage_path('app\public\landmark'));
+            $this->deleteImage($landmark->external_image, storage_path('app\public\landmark'));
+
+            foreach ($landmark->images as $image) {
+                // delete image from Image 
+                $this->deleteImage($image->path, storage_path('app\public\landmark'));
+            }
+
+            
             $landmark->delete();
-            return $this->successResponse(null,'deleted successfully', 200);
+            return $this->successResponse(null, 'deleted successfully', 200);
         } catch (\Throwable $th) {
             Log::error($th);
-            return $this->errorResponse(null,"there is something wrong in server",500);
+            return $this->errorResponse(null, "there is something wrong in server", 500);
         }
     }
 }
