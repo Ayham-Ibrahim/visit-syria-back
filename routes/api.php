03cd6b28fc0299983e7f40bfc0801709fb577c89
Ahmad-mh1999 <ahmedmhmad021@gmail.com>
2024-06-27 01:23:03 +0300

    user controller

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\HotelController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -30,8 +31,9 @@ Route::group([
     });
 Route::middleware('auth:api')->post('/logout', [AuthController::class, 'logout']);
 Route::middleware('auth:api')->put('/update-user/{user}', [AuthController::class, 'update']);
-
-
+Route::middleware('auth:api')->put('/admin-update/{user}', [UserController::class, 'updateAdmin']);
+Route::middleware('auth:api')->put('/admin-update-photo/{user}', [UserController::class, 'updateAdminImage']);
+Route::middleware('auth:api')->delete('/admin-delete-photo/{user}', [UserController::class, 'deleteAdminImage']);
 
 Route::apiResource('cities',CityController::class);
 Route::apiResource('services',CityController::class);
